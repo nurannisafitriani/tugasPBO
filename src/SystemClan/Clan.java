dd412eddcd245d6323f0d6b07143c943d845ae09
Nur Annisa Fitriani <nurannisa12388@gmail.com>
2017-05-20 21:05:56 +0700

    Update Clan.java

--- src/SystemClan/Clan.java
+++ src/SystemClan/Clan.java
@@ -7,7 +7,7 @@ public class Clan {
    private int kekuatan;
    private int jumlahanggota;
 
-    public Clan(int peringkat, String namaclan, int level, int kekuatan, int jumlahanggota) {
+    public Clan(int peringkat, String namaclan, int level, int kekuatan, int jumlahanggota) { //deklarasi variabel
         this.peringkat = peringkat;
         this.namaclan = namaclan;
         this.level = level;
@@ -15,22 +15,18 @@ public class Clan {
         this.jumlahanggota = jumlahanggota;
     }
 
-    public int getPeringkat() {
+    public int getPeringkat() { //method getter
         return peringkat;
     }
-
     public String getNamaclan() {
         return namaclan;
     }
-
     public int getLevel() {
         return level;
     }
-
     public int getKekuatan() {
         return kekuatan;
     }
-
     public int getJumlahanggota() {
         return jumlahanggota;
     }
